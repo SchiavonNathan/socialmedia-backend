9fb3714a52fea4986f01b324837a780b5ae5753b
SchiavonNathan <Nathanmestrepk@gmail.com>
2024-11-13 10:54:21 -0300

    atualizacao de login com google

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,20 +3,7 @@ import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Public } from './auth/constants';
 
-@Controller('google')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Public()
-  @Get()
-  @UseGuards(AuthGuard('google'))
-  async googleAuth(@Req() req) {}
-
-
-  @Public()
-  @Get('redirect')
-  @UseGuards(AuthGuard('google'))
-  googleAuthRedirect(@Req() req) {
-    return this.appService.googleLogin(req)
-  }
 }
